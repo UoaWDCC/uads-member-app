0f2a831ee07be46789b758d1d1946da03a8ad0e4
Daniel Li <danielli12357@gmail.com>
2021-08-07 17:41:37 +1200

    [Add] TS interface now also generated automatically via script

---
+++ api.ts
@@ -0,0 +1,27 @@
+import fetch from "node-fetch";
+import * as writeYamlFile from "write-yaml-file";
+import openapiTS from "openapi-typescript";
+import * as fs from "fs";
+
+async function downloadJson(): Promise<void> {
+  const result = fetch(
+    "https://studio-ws.apicur.io/sharing/a3f1a4a5-21ba-447d-849d-f6babb881956?content=true"
+  );
+
+  result
+    .then((res) => res.json())
+    .then((json) => {
+      writeYamlFile("api.yaml", json);
+    })
+    .then(() => {
+      openapiTS("api.yaml").then((res) => {
+        fs.writeFile("./src/interface/api.ts", res, (err) => {
+          if (err) {
+            console.log("Error, please try again");
+          }
+        });
+      });
+    });
+}
+
+downloadJson();
--- src/schema/PointSchema.ts
+++
@@ -1,6 +0,0 @@
-import * as Joi from 'joi';
-
-export const point2D = Joi.object({
-  x: Joi.string(),
-  y: Joi.number,
-}).meta({ className: 'Point' });
