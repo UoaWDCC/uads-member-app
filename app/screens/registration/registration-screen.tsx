611b3dae471204db8f3bcd5ec3e457f06bd4e0e4
viresh-oberoi <o.viresh@outlook.com>
2021-08-24 11:41:21 +1200

    [uads-009] Initialised registration screen

---
+++ app/screens/registration/registration-screen.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { observer } from "mobx-react-lite"
+import { ViewStyle } from "react-native"
+import { Screen, Text } from "../../components"
+// import { useNavigation } from "@react-navigation/native"
+// import { useStores } from "../../models"
+import { color } from "../../theme"
+
+const ROOT: ViewStyle = {
+  backgroundColor: color.background,
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  alignContent: "center"
+}
+
+export const RegistrationScreen = observer(function RegistrationScreen() {
+  // Pull in one of our MST stores
+  // const { someStore, anotherStore } = useStores()
+
+  // Pull in navigation via hook
+  // const navigation = useNavigation()
+  return (
+    <Screen style={ROOT} preset="scroll">
+      <Text preset="header" text="Registration" />
+      
+    </Screen>
+  )
+})
