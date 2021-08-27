92f8a1b543e3848d4238fb79057fa8b46db23d59
John Chen <john.bm.chen@gmail.com>
2021-08-27 16:29:14 +1200

    started GET_USER functionality

--- src/application/controller/UserController.ts
+++ src/application/controller/UserController.ts
@@ -3,9 +3,19 @@ import { NextFunction, Request, Response } from 'express';
 
 class UserController extends BaseController {
   async getUsers(req: Request, res: Response) {
-    console.log("Query<Club>: " + req.query.club);
+    var club = req.query.club;
+
+    if (club != null){
+      console.log("Getting By Clubs");
+      console.log("Query<Club>: " + club);
+    } else { 
+      console.log("Getting All");
+    }
+
     res.status(200).json([{ uuid: "SomeClub" }]);
   }
+
+
 }
 
 export { UserController };
