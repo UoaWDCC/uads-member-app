4cbc10cfbfa02b0b6c16ea76c3dad8d3f1a482e9
Daniel Li <danielli12357@gmail.com>
2021-08-31 16:28:21 +1200

    [Add] Mongoose Adapter

--- src/application/controller/EventController.ts
+++ src/application/controller/EventController.ts
@@ -1,9 +1,30 @@
 import { BaseController } from './BaseController';
 import { NextFunction, Request, Response } from 'express';
+import { MongoAdapter } from '../../infrastructure/MongoAdapter';
+import { ConsoleTransportOptions } from 'winston/lib/winston/transports';
 
 class EventController extends BaseController {
   async getEvents(req: Request, res: Response) {
-    res.status(200).json([{ name: 'hello world' }]);
+    const mongoAdapter = MongoAdapter.getInstance();
+
+    res.status(200).json([
+      {
+        uuid: '457eb090-ff13-4b9f-a6d1-33595e77f753',
+        date: '2000-91-22',
+        name: 'Event Name',
+        desc: 'Event Description',
+        'location:': {
+          building: '405',
+          room: '999',
+          'description:': 'Description of location',
+        },
+        imagePath:
+          'https://images.unsplash.com/photo-1599785209796-786432b228bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=80',
+        attendanceCap: '100',
+        sponsor: 'WDCC',
+        url: 'https://wdcc.co.nz/',
+      },
+    ]);
   }
 }
 
--- src/infrastructure/MongoAdapter.ts
+++ src/infrastructure/MongoAdapter.ts
@@ -1,5 +1,6 @@
 import { Db, MongoClient } from 'mongodb';
 import { logger } from '../util/Logger';
+import { Mongoose, createConnection, connections, Connection } from 'mongoose';
 
 /**
  * A singleton adapter that allows access to MongoDB cluster through a unique mongo uri.
@@ -13,36 +14,64 @@ class MongoAdapter {
   /**
    * Private MongoClient for purposes of getting another database from the same adapter instance.
    */
-  private client: MongoClient;
+  private client: Mongoose;
+
+  private openConnections: string[];
+
+  private uri: string;
 
   private static _instance: MongoAdapter;
 
-  private constructor(uri: string, dbName: string) {
-    const client = new MongoClient(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+  private constructor(uri: string) {
+    const mongoose = new Mongoose();
 
-    client.connect((err) => {
-      if (err) throw err;
-      this.db = client.db(dbName);
-      logger.info('MongoDB connected');
-    });
+    this.uri = uri;
+    this.openConnections = [];
+    // const client = new MongoClient(uri, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // });
+    // const client = new Mongoose();
+    // // client.connect(uri, { useNewUrlParser: true });
+    // client
+    //   .connect(uri, {})
+    //   .then(() => {
+    //     logger.info("MongoDB connected");
+    //   })
+    //   .catch((err) => {
+    //     logger.info(err);
+    //     throw err;
+    //   });
+    // client.connect((err) => {
+    //   if (err) throw err;
+    //   this.db = client.db(dbName);
+    //   logger.info("MongoDB connected");
+    // });
+    // this.client = client;
 
-    this.client = client;
+    this.client = mongoose;
   }
 
   public getDb(dbName: string): Db {
-    return this.client.db(dbName);
+    const index = this.openConnections.indexOf(dbName);
+    if (index != -1) {
+      return this.client.connections[index].db;
+    } else {
+      createConnection().openUri(this.uri, (err, connection) => {
+        if (err) throw err;
+        this.openConnections.push(dbName);
+        return connection.db;
+      });
+    }
   }
 
   /**
    * Builds a MongoAdapter using a `uri` and default `dbName`.
    */
-  public static build(uri: string, dbName: string): MongoAdapter {
+  public static build(uri: string): MongoAdapter {
     if (this._instance) throw new Error('MongoAdapter already built!');
 
-    this._instance = new this(uri, dbName);
+    this._instance = new this(uri);
     return this._instance;
   }
 
