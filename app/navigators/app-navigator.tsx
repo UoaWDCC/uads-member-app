8f251cdbe5548e4913fd09ad1b3d3d7ac445e1ed
Jose Santos <67150374+jose-vs@users.noreply.github.com>
2021-09-01 23:04:43 +1200

    fixed yarn.lock file

--- app/navigators/app-navigator.tsx
+++ app/navigators/app-navigator.tsx
@@ -7,7 +7,7 @@
 import React from "react"
 import { NavigationContainer, NavigationContainerRef } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { 
   LoginScreen, 
   AboutScreen,
