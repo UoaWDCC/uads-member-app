a46014056b382be8695d4f3430837e8fb2a4907a
Serena Lau (slau981) <serena.lau272@gmail.com>
2021-09-04 15:15:11 +1200

    implemented skeleton of POST function

--- src/application/controller/UserController.ts
+++ src/application/controller/UserController.ts
@@ -15,6 +15,28 @@ class UserController extends BaseController {
     res.status(200).json([{ uuid: "SomeClub" }]);
   }
 
+  async getUserByID(req: Request, res:Response){
+    var id = req.params.id
+    console.log(id)
+
+    res.status(200).json([{ uuid: "SomeClub2" }]);
+  }
+  
+  async createUser(req: Request, res:Response) {
+    const data = req.body;
+    console.log(data);
+
+    res.status(201).json(data);
+  }
+
+  async modifyUser(req: Request, res:Response){
+    const data = req.body;
+    console.log(data);
+  }
+
+  async deleteUser(req: Request, res: Response){
+    
+  }
 
 }
 
