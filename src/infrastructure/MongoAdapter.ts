4364e1efd569a753d4f674e00405ab5ae4266253
John Chen <john.bm.chen@gmail.com>
2021-09-30 19:18:53 +1300

    line 48 Mongo

--- src/infrastructure/MongoAdapter.ts
+++ src/infrastructure/MongoAdapter.ts
@@ -45,12 +45,10 @@ class MongoAdapter {
       res = this.client.connections[index].db;
       callBack(null, res);
     } else {
-      createConnection(this.uri, { dbName: dbName })
-        .asPromise()
-        .then((connection) => {
-          res = connection.db;
-          callBack(null, res);
-        });
+      createConnection(this.uri, { dbName: dbName }).then((connection) => {
+        res = connection.db;
+        callBack(null, res);
+      });
     }
   }
 
