c888581e3539732abf616fb7bfcd822ddfb51633
Daniel Li <danielli12357@gmail.com>
2021-10-01 23:20:58 +1300

    [Add] Refactored url validation

--- src/util/ValidationCheck.ts
+++ src/util/ValidationCheck.ts
@@ -1,8 +1,15 @@
-function checkValidUrl(url: string): boolean {
+import { components } from '../interface/api'
+
+function checkValidUrl(SocialHandle: components['schemas']['Socials']): boolean {
+  if (SocialHandle == null) {
+    return true;
+  }
+
   const regex = new RegExp(
     /https?:\/\/(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
   );
-  if (regex.test(url) || url == '') {
+
+  if (SocialHandle.url == '' || regex.test(SocialHandle.url)) {
     return true;
   }
   return false;
