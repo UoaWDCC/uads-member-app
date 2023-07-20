ddb6c0d30984d27344801f13a88faab58191a215
sunnyysetia <sunnyysetia@gmail.com>
2023-07-20 15:12:43 +1200

    Initial changes

--- client/app/screens/sponsors/sponsors-screen.tsx
+++ client/app/screens/sponsors/sponsors-screen.tsx
@@ -1,23 +1,18 @@
-import React, { useState, useEffect, useRef } from "react"
-import { observer } from "mobx-react-lite"
-import { ViewStyle, TextStyle, TouchableOpacity } from "react-native"
+import { Box, FlatList, NativeBaseProvider } from "native-base"
+import React, { useEffect, useState } from "react"
 import { Screen, SponsorIcon } from "../../components"
-import { useNavigation } from "@react-navigation/native"
-import { Text } from "../../components"
+import { TextStyle, TouchableOpacity, ViewStyle } from "react-native"
 import { color, typography } from "../../theme"
-import { NativeBaseProvider, Box, FlatList, Input, HStack } from "native-base"
-import firebase from "firebase"
-import axios from "axios"
-import Icon from "react-native-vector-icons/FontAwesome"
 
 import { BASE_URL } from "@env"
+import axios from "axios"
+import firebase from "firebase"
+import { observer } from "mobx-react-lite"
 
 export const SponsorsScreen = observer(function SponsorsScreen() {
   const navigation = useNavigation()
   const [sponsors, setSponsors] = useState([])
 
-  const searchInputRef = useRef<HTMLInputElement | null>(null)
-  const [query, setQuery] = useState("")
   useEffect(() => {
     firebase
       .auth()
@@ -31,6 +26,7 @@ export const SponsorsScreen = observer(function SponsorsScreen() {
           })
           .then(({ data }) => {
             data.sort((a, b) => (a.tier > b.tier ? 1 : b.tier > a.tier ? -1 : 0))
+            console.log(data)
             setSponsors(data)
           })
           .catch((e) => {
@@ -39,55 +35,74 @@ export const SponsorsScreen = observer(function SponsorsScreen() {
       })
   }, [])
 
+  const renderSponsor = ({ item }) => {
+    return (
+      <Box style={SPONSORICON}>
+        <Box
+          style={{
+            width: "100%",
+            padding: "1rem",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={{ uri: item.imageLink }}
+            style={{
+              width: sWidth * 0.85,
+              height: sHeight * 0.04871794871,
+              resizeMode: "stretch",
+            }}
+          />
+          <Box
+            style={{
+              position: "absolute",
+              top: "50%",
+            }}
+          >
+            <Text
+              style={{
+                paddingLeft: 10,
+              }}
+            >
+              {item.sponsorName}
+            </Text>
+          </Box>
+          <Box
+            style={{
+              backgroundColor: color.palette.palePeach,
+              position: "absolute",
+              zIndex: 1,
+              top: "50%",
+              right: 40,
+              height: 20,
+              width: 20,
+              borderRadius: "50%",
+              justifyContent: "center",
+              alignItems: "center",
+              transform: [{ translateY: "-50%" }],
+            }}
+          >
+            <Text style={{ fontWeight: "bold", paddingBottom: 1 }}>+</Text>
+          </Box>
+        </Box>
+      </Box>
+    )
+  }
+
+  // Search box
+  const [searchText, setSearchText] = useState("")
+
   return (
     <Screen style={ROOT} preset="scroll">
       <NativeBaseProvider>
         <Text style={TEXT} text={"Sponsors:"} />
-        <HStack space={2} alignItems="center">
-          <Input
-            ref={searchInputRef}
-            onChangeText={(text) => setQuery(text)}
-            // eslint-disable-next-line react-native/no-inline-styles
-            // style={{ width: "calc(90vw - 30px)", height: 38 }}
-            borderRadius="40px"
-            marginLeft="10px"
-            float="left"
-            placeholder="Search"
-            _light={{
-              placeholderTextColor: color.text,
-              backgroundColor: color.palette.white,
-              borderColor: color.palette.goldenGlow,
-            }}
-            _dark={{
-              placeholderTextColor: color.text,
-            }}
-          />
-          <Icon name="search" size={20} color="#333" width="fit-content" />
-        </HStack>
-
         <Box style={CONTAINER}>
           <FlatList
             data={sponsors}
-            numColumns={3}
+            numColumns={1}
             keyExtractor={(item, index) => item.uuid}
-            renderItem={({ item }) => {
-              const { sponsorName, uuid, imageLink } = item
-              const prop = {
-                name: `${sponsorName}`,
-                imgUrl: imageLink,
-              }
-              return (
-                <Box style={SPONSORICON}>
-                  <TouchableOpacity
-                    onPress={() => {
-                      navigation.navigate("sponsor", item)
-                    }}
-                  >
-                    <SponsorIcon key={uuid} props={prop} />
-                  </TouchableOpacity>
-                </Box>
-              )
-            }}
+            renderItem={renderSponsor}
           />
         </Box>
       </NativeBaseProvider>
@@ -100,18 +115,9 @@ const ROOT: ViewStyle = {
   flex: 1,
 }
 
-const TEXT: TextStyle = {
-  fontFamily: typography.primary,
-  fontWeight: "bold",
-  fontSize: 40,
-  color: color.text,
-  marginTop: 30,
-  marginLeft: 30,
-}
-
 const CONTAINER: ViewStyle = {
   flex: 1,
-  backgroundColor: "#fffffa",
+  backgroundColor: color.background,
   marginTop: 10,
   borderTopRightRadius: 40,
   borderTopLeftRadius: 40,
@@ -120,4 +126,5 @@ const CONTAINER: ViewStyle = {
 const SPONSORICON: ViewStyle = {
   flexDirection: "column",
   margin: 1,
+  justifyContent: "center",
 }
--- client/app/theme/palette.ts
+++ client/app/theme/palette.ts
@@ -1,17 +1,12 @@
 export const palette = {
   black: "#1d1d1d",
   white: "#ffffff",
-  offWhite: "#e6e6e6",
-  orange: "#FBA928",
-  orangeDarker: "#EB9918",
-  lightGrey: "#939AA4",
-  lighterGrey: "#CDD4DA",
-  angry: "#dd3333",
-  deepPurple: "#5D2555",
-  earlyDawn: "#fff4de", // background color (primary colour)
-  goldenGlow: "#ffd788", // secondary color
-  tangelo: "#ffad70", // sign-in button colour (tertiary color)
-  brown: "#503106", // text color
-  popupGrey: "#EAE7E7",
-  grey: "#A09D9D",
+  sand: "#e7c9bf",
+  palePeach: "#eac3bc",
+  lilac: "#d9b5c1",
+  dustyPink: "#b96574",
+  fuschia: "#c44f6c",
+  twentyFiveFuschia: "rgba(196, 79, 108, 0.25)",
+  brown: "#804949",
+  darkRed: "#801e15",
 }
