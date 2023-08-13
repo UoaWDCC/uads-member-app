73098ec12481b5f2a49841554adcf58270b34c56
Ruth Gao <zgao258@aucklanduni.ac.nz>
2023-08-14 01:15:37 +1200

    Side bar styling, items evently across

--- client/app/components/side-drawer/side-drawer.tsx
+++ client/app/components/side-drawer/side-drawer.tsx
@@ -1,82 +1,64 @@
 import React from "react"
-import { View, StyleSheet } from "react-native"
+import { View, StyleSheet, Text, Dimensions } from "react-native"
 import { DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer"
 
 export function DrawerContent(props) {
+  const drawerItemData = [
+    { label: "Events", screen: "events" },
+    { label: "Sponsors\n&Offers", screen: "offers" },
+    { label: "About Us", screen: "about" },
+    { label: "Settings", screen: "settings" },
+  ]
+
+  const windowHeight = Dimensions.get("window").height
+  const containerHeight = Math.round(windowHeight / drawerItemData.length)
+
   return (
     <View style={{ flex: 1 }}>
       <DrawerContentScrollView {...props}>
-        <DrawerItem
-          label="About Us"
-          onPress={() => {
-            props.navigation.navigate("about")
-          }}
-        />
-        <DrawerItem
-          label="Events"
-          onPress={() => {
-            props.navigation.navigate("events")
-          }}
-        />
-        <DrawerItem
-          label="Offers"
-          onPress={() => {
-            props.navigation.navigate("offers")
-          }}
-        />
-        <DrawerItem
-          label="settings"
-          onPress={() => {
-            props.navigation.navigate("settings")
-          }}
-        />
+        {drawerItemData.map((item, index) => (
+          <DrawerItem
+            key={index}
+            label={() => (
+              <Text style={[styles.drawerItem, styles.drawerItemText]}>
+                {item.label}
+              </Text>
+            )}
+            onPress={() => {
+              props.navigation.navigate(item.screen)
+            }}
+            style={[
+              styles.drawerItemContainer,
+              {
+                height: containerHeight,
+              },
+              {
+                top: (windowHeight - containerHeight * drawerItemData.length) / 2 + index * containerHeight,
+              },
+            ]}
+          />
+        ))}
       </DrawerContentScrollView>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  drawerContent: {
-    flex: 1,
+  drawerItem: {
+    fontFamily: "Bitter",
+    fontSize: 48,
+    fontStyle: "italic",
+    fontWeight: "700",
+    lineHeight: 58,
+    letterSpacing: 0,
+    textAlign: "center",
   },
-  userInfoSection: {
-    paddingLeft: 20,
-  },
-  title: {
-    fontSize: 16,
-    marginTop: 3,
-    fontWeight: "bold",
-  },
-  caption: {
-    fontSize: 14,
-    lineHeight: 14,
-  },
-  row: {
-    marginTop: 20,
+  drawerItemContainer: {
+    position: "absolute",
     flexDirection: "row",
     alignItems: "center",
   },
-  section: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginRight: 15,
-  },
-  paragraph: {
-    fontWeight: "bold",
-    marginRight: 3,
-  },
-  drawerSection: {
-    marginTop: 15,
-  },
-  bottomDrawerSection: {
-    marginBottom: 15,
-    borderTopColor: "#f4f4f4",
-    borderTopWidth: 1,
-  },
-  preference: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingVertical: 12,
-    paddingHorizontal: 16,
+  drawerItemText: {
+    color: "#801E15", // Text color
   },
 })
