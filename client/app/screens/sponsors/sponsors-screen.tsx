0a65e59d1471984fc79472f8e9282e862e3324da
Jolin <jche521@aucklanduni.ac.nz>
2023-08-14 12:12:24 +1200

    U2-43: added menu button to sponsors screen

--- client/app/screens/sponsors/sponsors-screen.tsx
+++ client/app/screens/sponsors/sponsors-screen.tsx
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle, TextStyle, TouchableOpacity } from "react-native"
-import { Screen, SponsorIcon } from "../../components"
-import { useNavigation } from "@react-navigation/native"
+import { ViewStyle, TextStyle, TouchableOpacity, StyleSheet } from "react-native"
+import { Button, Screen, SponsorIcon, AutoImage as Image } from "../../components"
 import { Text } from "../../components"
 import { color, typography } from "../../theme"
 import { NativeBaseProvider, Box, FlatList } from "native-base"
 import firebase from "firebase"
 import axios from "axios"
 import { BASE_URL } from "@env"
+import { TabNavigatorParamList } from "../../navigators"
+import { DrawerNavigationProp } from "@react-navigation/drawer"
 
-export const SponsorsScreen = observer(function SponsorsScreen() {
-  const navigation = useNavigation()
+const menuIcon = require("../../resources/menu-icon.svg")
+
+interface SponsorsScreenProps {
+  navigation: DrawerNavigationProp<TabNavigatorParamList, "events">
+}
+
+export const SponsorsScreen = observer(function SponsorsScreen({
+  navigation,
+}: SponsorsScreenProps) {
   const [sponsors, setSponsors] = useState([])
 
   useEffect(() => {
@@ -38,6 +46,15 @@ export const SponsorsScreen = observer(function SponsorsScreen() {
   return (
     <Screen style={ROOT} preset="scroll">
       <NativeBaseProvider>
+        <Button
+          onPress={() => {
+            // Handle press
+            navigation.openDrawer()
+          }}
+          style={styles.menuBtnStyle}
+        >
+          <Image source={menuIcon} style={styles.iconStyle} />
+        </Button>
         <Text style={TEXT} text={"Sponsors:"} />
         <Box style={CONTAINER}>
           <FlatList
@@ -95,3 +112,17 @@ const SPONSORICON: ViewStyle = {
   flexDirection: "column",
   margin: 1,
 }
+
+const styles = StyleSheet.create({
+  iconStyle: {
+    height: 30,
+    width: 30,
+  },
+  menuBtnStyle: {
+    padding: 20, // Increase padding to make the button bigger
+    position: "fixed", // Position it at the top left corner
+    top: 10,
+    left: 10,
+    zIndex: 10,
+  },
+})
